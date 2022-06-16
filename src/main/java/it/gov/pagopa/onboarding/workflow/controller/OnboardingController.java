b7364560e819cfeed433515d7220c3bba37c2fd8
dpellicc <dpellicc@everis.com>
2022-06-16 21:53:34 +0200

    change ingress path and requestmapping

--- src/main/java/it/gov/pagopa/onboarding/workflow/controller/OnboardingController.java
+++ src/main/java/it/gov/pagopa/onboarding/workflow/controller/OnboardingController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("/onboarding")
+@RequestMapping("/idpay/onboarding")
 public interface OnboardingController {
 
   /**
