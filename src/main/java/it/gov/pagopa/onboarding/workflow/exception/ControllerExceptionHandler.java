00194f718a9c5269c4aea905d466000264b09cdd
Marco Di Lisa <marco.di.lisa@nttdata.com>
2023-02-23 11:32:50 +0100

    Error logs

--- src/main/java/it/gov/pagopa/onboarding/workflow/exception/ControllerExceptionHandler.java
+++ src/main/java/it/gov/pagopa/onboarding/workflow/exception/ControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.onboarding.workflow.exception;
 import it.gov.pagopa.onboarding.workflow.dto.ErrorDTO;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -11,10 +12,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class ControllerExceptionHandler {
 
   @ExceptionHandler({OnboardingWorkflowException.class})
   public ResponseEntity<ErrorDTO> handleException(OnboardingWorkflowException ex) {
+    log.error("[ONBOARDING_WORKFLOW][{}] New excpetion: {}", HttpStatus.valueOf(ex.getCode()), ex.getMessage());
     return new ResponseEntity<>(new ErrorDTO(ex.getCode(), ex.getMessage()),
         HttpStatus.valueOf(ex.getCode()));
   }
@@ -29,6 +32,7 @@ public class ControllerExceptionHandler {
       errors.add(String.format("[%s]: %s", fieldName, errorMessage));
     });
     String message = String.join(" - ", errors);
+    log.error("[ONBOARDING_WORKFLOW][{}] New excpetion: {}", HttpStatus.BAD_REQUEST, message);
     return new ResponseEntity<>(
         new ErrorDTO(HttpStatus.BAD_REQUEST.value(), message),
         HttpStatus.BAD_REQUEST);
