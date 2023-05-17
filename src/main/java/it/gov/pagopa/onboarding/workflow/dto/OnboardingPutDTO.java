2c6b8369f631c7f27508314435410b2fbe20249d
marco-di-lisa <105736613+marco-di-lisa@users.noreply.github.com>
2023-05-17 16:17:53 +0200

    feat: update to Spring Boot 3 (#85)
    * [IDP-765] Spring Boot 3
    
    * [IDP-765] Spring Boot 3 - Janino

--- src/main/java/it/gov/pagopa/onboarding/workflow/dto/OnboardingPutDTO.java
+++ src/main/java/it/gov/pagopa/onboarding/workflow/dto/OnboardingPutDTO.java
@@ -1,6 +1,6 @@
 package it.gov.pagopa.onboarding.workflow.dto;
 
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
