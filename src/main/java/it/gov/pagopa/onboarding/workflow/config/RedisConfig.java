50873984f430c7bd9c12008ae43125382b53aa1d
anttorre <antonio.torre@nttdata.com>
2023-06-09 15:22:16 +0200

    IDP-1128 extractingRuCost PutTC

--- src/main/java/it/gov/pagopa/onboarding/workflow/config/RedisConfig.java
+++ src/main/java/it/gov/pagopa/onboarding/workflow/config/RedisConfig.java
@@ -2,13 +2,14 @@ package it.gov.pagopa.onboarding.workflow.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.gov.pagopa.onboarding.workflow.dto.initiative.InitiativeDTO;
-import java.time.Duration;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -18,8 +19,7 @@ public class RedisConfig {
     ObjectMapper mapper = new ObjectMapper();
     mapper.findAndRegisterModules();
 
-    Jackson2JsonRedisSerializer<InitiativeDTO> serializer = new Jackson2JsonRedisSerializer<>(InitiativeDTO.class);
-    serializer.setObjectMapper(mapper);
+    Jackson2JsonRedisSerializer<InitiativeDTO> serializer = new Jackson2JsonRedisSerializer<>(mapper, InitiativeDTO.class);
 
     return builder -> builder
         .withCacheConfiguration("initiativeBeneficiaryView",
