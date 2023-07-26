d36bd574d05ec7dac88b5cab721b8cd68bddaf7f
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2023-07-26 14:25:14 +0200

    chore: Onboarding integration test (#139)
    * integrationTest of entire onboarding API flow
    
    * testing complete onboarding

--- src/test/java/it/gov/pagopa/onboarding/workflow/controller/OnboardingControllerIntegrationTest.java
+++ src/test/java/it/gov/pagopa/onboarding/workflow/controller/OnboardingControllerIntegrationTest.java
@@ -1,19 +1,17 @@
 package it.gov.pagopa.onboarding.workflow.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-
 import it.gov.pagopa.common.mongo.MongoTestUtilitiesService;
+import it.gov.pagopa.common.utils.TestUtils;
 import it.gov.pagopa.common.web.mockvc.MockMvcUtils;
 import it.gov.pagopa.onboarding.workflow.BaseIntegrationTest;
 import it.gov.pagopa.onboarding.workflow.connector.InitiativeRestClient;
 import it.gov.pagopa.onboarding.workflow.connector.admissibility.AdmissibilityRestClient;
 import it.gov.pagopa.onboarding.workflow.constants.OnboardingWorkflowConstants;
-import it.gov.pagopa.onboarding.workflow.dto.OnboardingPutDTO;
+import it.gov.pagopa.onboarding.workflow.dto.*;
 import it.gov.pagopa.onboarding.workflow.model.Onboarding;
 import it.gov.pagopa.onboarding.workflow.repository.OnboardingRepository;
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
+import org.apache.commons.lang3.function.FailableConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -22,9 +20,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+
+@TestPropertySource(properties = {
+        "logging.level.it.gov.pagopa.onboarding.workflow.service.OnboardingServiceImpl=WARN"
+})
 class OnboardingControllerIntegrationTest extends BaseIntegrationTest {
+
+    public static final String INITIATIVE_ID = "INITIATIVE_ID";
+    public static final String USERID_FORMAT = "USERID%d";
 
     @Autowired
     private OnboardingRepository onboardingRepository;
@@ -37,7 +50,7 @@ class OnboardingControllerIntegrationTest extends BaseIntegrationTest {
     private final Set<String> onboardingTestIds = new HashSet<>();
 
     @AfterEach
-    void clearTestData(){
+    void clearTestData() {
         onboardingRepository.deleteAllById(onboardingTestIds);
     }
 
@@ -51,37 +64,111 @@ class OnboardingControllerIntegrationTest extends BaseIntegrationTest {
                 )
                 .andReturn();
     }
+
+    protected MvcResult onboardingStatus(String userId, String initiativeId) throws Exception {
+        onboardingTestIds.add(userId);
+        return mockMvc
+                .perform(get("/idpay/onboarding/{initiativeId}/{userId}/status", initiativeId, userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andReturn();
+    }
+
+    protected MvcResult checkPrerequisites(String userId, String initiativeId) throws Exception {
+        onboardingTestIds.add(userId);
+        return mockMvc
+                .perform(put("/idpay/onboarding/initiative/{userId}", userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new CheckPutDTO(initiativeId, "CHANNEL")))
+                )
+                .andReturn();
+    }
+
+    protected MvcResult saveConsent(String userId, String initiativeId, List<SelfConsentDTO> consents) throws Exception {
+        onboardingTestIds.add(userId);
+        return mockMvc
+                .perform(put("/idpay/onboarding/consent/{userId}", userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new ConsentPutDTO(initiativeId, true, consents)))
+                )
+                .andReturn();
+    }
 //endregion
 
     @Test
-    void testOnboardingCitizen() throws Exception {
+    void testOnboardingCitizen() {
         // Given
-        int N=10;
-        String userIdFormat = "USERID%d";
-        String initiativeId = "INITIATIVE_ID";
+        int N = 10;
+        int parallelism = 5;
 
         // When
         MongoTestUtilitiesService.startMongoCommandListener("OnboardingNewCitizen");
-        for (int i = 0; i < N; i++) {
-            MockMvcUtils.extractResponse(onboardingCitizen(userIdFormat.formatted(i), initiativeId), HttpStatus.NO_CONTENT, null);
-        }
+        TestUtils.executeParallelUseCases(N, useCases, parallelism);
         MongoTestUtilitiesService.stopAndPrintMongoCommands();
 
         // Then
         for (int i = 0; i < N; i++) {
-            Onboarding result = onboardingRepository.findById(Onboarding.buildId(initiativeId, userIdFormat.formatted(i))).orElse(null);
+            Onboarding result = onboardingRepository.findById(Onboarding.buildId(INITIATIVE_ID, USERID_FORMAT.formatted(i))).orElse(null);
             Assertions.assertNotNull(result);
-            Assertions.assertEquals(OnboardingWorkflowConstants.ACCEPTED_TC, result.getStatus());
+            Assertions.assertEquals(OnboardingWorkflowConstants.ON_EVALUATION, result.getStatus());
             Assertions.assertTrue(result.getTc());
+            Assertions.assertEquals("CHANNEL", result.getChannel());
             assertTimestampValue(result.getTcAcceptTimestamp());
             assertTimestampValue(result.getUpdateDate());
         }
 
-        Mockito.verify(initiativeRestClientSpy).getInitiativeBeneficiaryView(initiativeId);
-        Mockito.verify(admissibilityRestClientSpy, Mockito.times(N)).getInitiativeStatus(initiativeId);
+        // cached client side
+        Mockito.verify(initiativeRestClientSpy, Mockito.atMost(parallelism)).getInitiativeBeneficiaryView(INITIATIVE_ID);
+        // executed both when accepting T&C and checkPrerequisites
+        Mockito.verify(admissibilityRestClientSpy, Mockito.times(3 * N)).getInitiativeStatus(INITIATIVE_ID);
+
+        List<ConsumerRecord<String, String>> onboardingRequestNotify = kafkaTestUtilitiesService.consumeMessages(topicOnboarding1, N, 10000);
+        Assertions.assertEquals(N, onboardingRequestNotify.size());
     }
 
+//region useCases
+    private final List<FailableConsumer<Integer, Exception>> useCases = List.of(
+
+            // useCase0: successful onboarding on non-whitelist initiative
+            bias -> {
+                String userId = USERID_FORMAT.formatted(bias);
+
+                // Put T&C
+                MockMvcUtils.extractResponse(onboardingCitizen(userId, INITIATIVE_ID), HttpStatus.NO_CONTENT, null);
+
+                // Get Status
+                assertOnboardingStatus(
+                        MockMvcUtils.extractResponse(onboardingStatus(userId, INITIATIVE_ID), HttpStatus.OK, OnboardingStatusDTO.class),
+                        OnboardingWorkflowConstants.ACCEPTED_TC);
+
+                // Put AcceptPrerequisites
+                MockMvcUtils.extractResponse(checkPrerequisites(userId, INITIATIVE_ID), HttpStatus.OK, RequiredCriteriaDTO.class);
+
+                //Put SaveConsent
+                MockMvcUtils.extractResponse(saveConsent(userId, INITIATIVE_ID, List.of()), HttpStatus.ACCEPTED, null);
+
+                // Get Status
+                assertOnboardingStatus(
+                        MockMvcUtils.extractResponse(onboardingStatus(userId, INITIATIVE_ID), HttpStatus.OK, OnboardingStatusDTO.class),
+                        OnboardingWorkflowConstants.ON_EVALUATION);
+
+            }
+    );
+//endregion
+
 //region assertion utils
+    private static void assertOnboardingStatus(OnboardingStatusDTO result, String expectedStatus) {
+        Assertions.assertNotNull(result);
+        assertTimestampValue(result.getStatusDate());
+        result.setStatusDate(null);
+
+        Assertions.assertEquals(
+                OnboardingStatusDTO.builder()
+                        .status(expectedStatus)
+                        .build(),
+                result);
+    }
+
     private static void assertTimestampValue(LocalDateTime result) {
         Assertions.assertNotNull(result);
         Assertions.assertTrue(result.isAfter(LocalDateTime.now().minusMinutes(1)));
