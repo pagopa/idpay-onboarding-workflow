b3426e8dfb55c67b1789bbdc7f9d3562a6efb5a2
andreaV95 <157486351+Vitolo-Andrea@users.noreply.github.com>
2025-11-10 10:28:17 +0100

    fix: [LTBE-95] assistance api (#363)

--- src/main/java/it/gov/pagopa/onboarding/workflow/controller/OnboardingController.java
+++ src/main/java/it/gov/pagopa/onboarding/workflow/controller/OnboardingController.java
@@ -45,6 +45,17 @@ public interface OnboardingController {
             @PathVariable("initiativeId") String initiativeId, @PathVariable("userId") String userId);
 
     /**
+     * Returns the actual onboarding status for assistance api
+     *
+     * @param initiativeId
+     * @param userId
+     * @return
+     */
+    @GetMapping("/{initiativeId}/{userId}/assistance")
+    ResponseEntity<OnboardingAssistanceDTO> onboardingStatusAssistance(
+            @PathVariable("initiativeId") String initiativeId, @PathVariable("userId") String userId);
+
+    /**
      * Save the consents of PDND criteria and Self declaration list
      *
      * @param consentPutDTO
