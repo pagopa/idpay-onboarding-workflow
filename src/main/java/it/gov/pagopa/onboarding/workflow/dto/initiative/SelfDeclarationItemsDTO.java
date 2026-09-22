59dc25809394906b2d472eab7830782281023fe9
v1tt0ri0Alt <valtieri@emeal.nttdata.com>
2026-09-22 11:25:14 +0200

    feat: [BND-1884] Adeguamento idpay-onboarding-workflow (#404)

--- src/main/java/it/gov/pagopa/onboarding/workflow/dto/initiative/SelfDeclarationItemsDTO.java
+++ src/main/java/it/gov/pagopa/onboarding/workflow/dto/initiative/SelfDeclarationItemsDTO.java
@@ -11,7 +11,8 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonSubTypes({
     @JsonSubTypes.Type(value = SelfCriteriaTextDTO.class, name = "text"),
     @JsonSubTypes.Type(value = SelfCriteriaBoolDTO.class, name = "boolean"),
-    @JsonSubTypes.Type(value = SelfCriteriaMultiTypeDTO.class, name = "multi_consent")
+    @JsonSubTypes.Type(value = SelfCriteriaMultiTypeDTO.class, name = "multi_consent"),
+    @JsonSubTypes.Type(value = SelfCriteriaInformativeDTO.class, name = "informative")
 })
 public interface SelfDeclarationItemsDTO {
 
